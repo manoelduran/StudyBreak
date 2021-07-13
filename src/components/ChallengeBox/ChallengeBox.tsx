4d74e35c86cdd475a682b72ebd7aba1cc823394d
Manoel Duran <manoel.duran@hotmail.com>
2021-07-13 12:37:41 -0300

    bug image

--- src/components/ChallengeBox/ChallengeBox.tsx
+++ src/components/ChallengeBox/ChallengeBox.tsx
@@ -44,7 +44,7 @@ export function ChallengeBox() {
         <div className={styles.challengeNotActive}>
           <strong>Comece a jogar para poder receber seus desafios!</strong>
           <p>
-            <img src="icons/levelup.png" width="100px" height="100px" alt="Level up" />
+            <Image src="/icons/levelup.png" width="100px" height="100px" alt="Level up" />
             Complete seus desafios para avançar de level!
           </p>
         </div>
